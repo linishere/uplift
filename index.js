3ce5febb4137b7e4df600c5ce0efce298a60ff7f
Lin <linmiyamoto@Apples-MacBook-Pro-3.local>
2019-07-19 09:43:29 -0700

    no u

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-* {
-  margin: 0;
-}
 
-@media all and (max-width: 900px) {
-
-}
-
-@media all and (max-width: 700px) {
-
-}
 
 var button = $('button');
 var inputdisplay = $('.inputdisplay')
@@ -22,31 +12,4 @@ function woo() {
 
 function gotobutton(text) {
   inputdisplay.append(`<p class="dietrate">${text}</p>`);
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = $("mySlides");
-  var dots = $("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
